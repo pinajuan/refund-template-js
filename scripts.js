aba0f04028eebb163f6d7346aa347ac76b331e63
Juan Pina <juanpinaribeiro@outlook.com>
2025-03-03 13:11:49 -0300

    Item: adicionando o "remover ítem"

--- scripts.js
+++ scripts.js
@@ -73,8 +73,14 @@ function expenseAdd(newExpense) {
         expenseAmount.classList.add("expense-amount")
         expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
 
+        // Cria o ícone de remover
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "remover")
+
         // Adiciona as informações no item
-        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
         // Adiciona o item em si na lista que será adicionada em "Minhas solicitações"
         expenseList.append(expenseItem)
