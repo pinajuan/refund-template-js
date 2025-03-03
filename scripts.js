1c16a20b69801b5edfd0fc7b2ded1d2d00156145
Juan Pina <juanpinaribeiro@outlook.com>
2025-03-03 09:29:45 -0300

    Input: formatando a moeda para o padrão BRL

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount")
 
+// Caputa o evento de input para formatar o valor
 amount.oninput = () => {
-    let value = amount.value.replace(/\D/g, "");
+    let value = amount.value.replace(/\D/g, "")
+    value = Number(value)/100
+    amount.value = formatCurrencyBRL(value)
+}
 
-    amount.value = value
+// Formata o valor no padrão BRL (Real brasileiro)
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+    return value
 }
