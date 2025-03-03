0e5c64338ec274eed833188f40c5211222ea4289
Juan Pina <juanpinaribeiro@outlook.com>
2025-03-03 14:17:49 -0300

    Item: percorrendo a lista de itens e somando os valores de cada um deles

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category")
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
+const expensesTotal = document.querySelector("aside header h2")
 const expensesQuantity = document.querySelector("aside header p span")
 
 // Captura o evento de input para formatar o valor
@@ -102,6 +103,28 @@ function updateTotals() {
         // Recupera todos os itens (li) da lista (ul)
         const items = expenseList.children
         expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+    
+        let total = 0
+
+        for(let item = 0; item < items.length; item++) {
+            const itemAmount = items[item].querySelector(".expense-amount")
+            console.log(itemAmount)
+
+            // Remove caracteres não numéricos e subtitui a vírgula por ponto
+            let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")
+
+            value = parseFloat(value)
+
+            if (isNaN(value)) {
+                return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+            }
+
+            total += Number(value)
+
+        }
+
+        expensesTotal.textContent = total
+    
     } catch (error) {
         console.log(error)
         alert("Não foi possível atualizar o total de itens.")
